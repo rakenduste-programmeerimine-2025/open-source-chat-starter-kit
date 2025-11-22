263bb517235775e79f35c26971e420c3c53b721f
marmarsell <marsell@tlu.ee>
2025-11-22 19:20:23 +0200

    refactor: did typescript shenanigans, messages are messages now. Changed visual appeal of message box too!

--- app/me/chat/page.tsx
+++ app/me/chat/page.tsx
@@ -4,9 +4,18 @@ import { CSSProperties, useEffect, useState } from "react";
 
 export default function Page() {
 
+  type Message = {
+    deleted_at: null
+    edited_at: null
+    id: number
+    message: string
+    sender_id: string
+    sent_on: string
+    server_id: string
+  }
 
-  const [chatList, setChatList] = useState([[{}]])
-    
+  const [chatList, setChatList] = useState([[{}]]) as unknown as Array<Array<Message>>
+
   useEffect(() => {
     fetch('http://localhost:3000/api/messages')
       .then(response => response.json())
@@ -42,7 +51,7 @@ export default function Page() {
 
   const scrolldivB = {
     display: "flex",
-    justifyContent: "space-around",
+    justifyContent: "start",
     flexDirection: "column",
     backgroundColor: "gray",
     height: "70vh"
@@ -63,22 +72,29 @@ export default function Page() {
         <div style={sidedivL}>
           <div style={scrolldivB}>
             this is a chat box
+            <div>server name {chatList[0].server_id}</div>
+            <hr style={{borderWidth: "1vh", borderColor: "black"}}/>
             {chatList.map((message, key)=>(
-                <div key={key}>{message.message}</div>
+                <div key={key}>
+                  <hr />
+                  sent at {message.sent_on}
+                  <div style={{backgroundColor: "darkgray"}}>{message.message}</div>
+                  <hr />
+                </div>
             ))}
           </div>
-          <hr style={{borderWidth: "1vh"}}/>
+          <hr style={{borderWidth: "1vh", borderColor: "black"}}/>
           <div style={scrolldivS}>
-            this is your servers
+            there is only this server
           </div>
         </div>
         <div style={sidedivR}>
           <div style={scrolldivS}>
-            this is your profile
+            you are the only user
           </div>
-          <hr style={{borderWidth: "1vh"}}/>
+          <hr style={{borderWidth: "1vh", borderColor: "black"}}/>
           <div style={scrolldivB}>
-            this is your friends
+            you have no friends
           </div>
         </div>
       </div>
