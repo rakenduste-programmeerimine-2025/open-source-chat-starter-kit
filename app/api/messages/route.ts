45b53bb29e97f1ab831fbecea9932e44f46c432e
ReDerEE <108543563+ReDerEE@users.noreply.github.com>
2025-11-22 19:03:56 +0200

    feat: GET messages api endpoint returns the username now

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -14,7 +14,7 @@ export async function POST(request: NextRequest) {
       sender_id: body.sender_id,
       message: body.message,
     })
-    return new Response(JSON.stringify({ message: body }), {
+    return new Response(JSON.stringify(body), {
       status: 200,
       headers: { "Content-Type": "application/json" },
     })
@@ -31,10 +31,29 @@ export async function GET(request: NextRequest) {
   //   if (body.server_id) {
   //     const { data, error } = supabase.from("messages").select().eq(id, server_id)
   //   } else {
-  const { data, error } = await supabase.from("messages").select()
+  const users = await getUsers()
+
+  const { data, error } = await supabase.from("messages").select("*")
+
+  for (let i = 0; i < data?.length; i++) {
+    data[i].username = users.find(
+      user => user.id === data[i].sender_id,
+    ).username
+  }
+
+  console.log(data)
+  console.log(users)
   return new Response(JSON.stringify({ data }), {
     status: 200,
     headers: { "Content-Type": "application/json" },
   })
 }
-// }
+
+async function getUsers() {
+  const { data, error } = await supabase.auth.admin.listUsers()
+  const users = (data?.users || []).map(user => ({
+    id: user.id,
+    username: user.user_metadata?.username || null, // or raw_user_meta_data if available
+  }))
+  return users
+}
