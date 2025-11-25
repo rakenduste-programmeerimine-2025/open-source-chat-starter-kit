1a56aa0498562232fa5a5aaf3eadfce4e2cd21e5
RenatMagsumov <renatik@tlu.ee>
2025-11-25 23:57:40 +0200

    feat(servers): add stub createServerAction (#8)

---
+++ app/(app)/servers/actions.ts
@@ -0,0 +1,8 @@
+"use server";
+
+/**
+ * Stub: will create a server (name, image_url).
+ */
+export async function createServerAction(_formData: FormData) {
+    throw new Error("Not implemented yet");
+}
