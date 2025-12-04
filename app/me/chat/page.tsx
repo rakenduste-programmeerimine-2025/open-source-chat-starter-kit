d8151c2c1f0fb58c3a33a6b7bc40ef58805bf349
marmarsell <marsell@tlu.ee>
2025-12-04 18:38:09 +0200

    feat: added ability to switch servers, fixed all errors in me/chat

--- app/me/chat/page.tsx
+++ app/me/chat/page.tsx
@@ -36,26 +36,35 @@ export default function Page() {
   }
 
   const [chatList, setChatList] = useState([[{}]]) as unknown as Array<Messages>
+  const [serverList, setServerList] = useState([[{}]]) as unknown as Array<ServList>
   const [serverData, setServerData] = useState([[{}]]) as unknown as Array<Array<ServData>>
 
-  const [serverID, setServerID] = useState("f291a9a9-10aa-4057-acdc-7e036d7111ac")
-  //const [serverID, setServerID] = useState("36d2abfa-2d13-48f9-b701-78e355c89f21")
+  //const [serverID, setServerID] = useState("f291a9a9-10aa-4057-acdc-7e036d7111ac")
+  const [serverID, setServerID] = useState("36d2abfa-2d13-48f9-b701-78e355c89f21")
 
   const [updateCounter, setUpdateCounter] = useState(0)
 
   useEffect(() => {
     fetch('http://localhost:3000/api/messages/' + serverID)
       .then(response => response.json())
-      .then(response => setChatList(response.data))
+      //@ts-expect-error because we can in fact call the function
+      .then(response => setChatList(response.data)) 
+
+    fetch('http://localhost:3000/api/servers')
+      .then(response => response.json())
+      //@ts-expect-error because we can in fact call the function
+      .then(response => setServerList(response))
 
     fetch('http://localhost:3000/api/servers/' + serverID)
       .then(response => response.json())
+      //@ts-expect-error because we can in fact call the function
       .then(response => setServerData(response))
   }, [serverID])
 
   useEffect(() => {
+    setUpdateCounter(updateCounter + 1)
     console.warn(updateCounter)
-    console.log(serverData, chatList)
+    console.log(serverData, serverList, chatList)
   }, [serverData && chatList])
   
 
@@ -70,7 +79,7 @@ export default function Page() {
     justifyContent: "space-around",
     flexDirection: "column",
     backgroundColor: "gray",
-    width: "60vw"
+    width: "60vw",
   } as CSSProperties
 
   const sidedivR = {
@@ -78,7 +87,7 @@ export default function Page() {
     justifyContent: "space-around",
     flexDirection: "column",
     backgroundColor: "gray",
-    width: "35vw"
+    width: "35vw",
   } as CSSProperties
 
   const scrolldivB = {
@@ -86,7 +95,7 @@ export default function Page() {
     justifyContent: "start",
     flexDirection: "column",
     backgroundColor: "gray",
-    height: "70vh"
+    height: "70vh",
   } as CSSProperties
 
   const scrolldivS = {
@@ -94,9 +103,12 @@ export default function Page() {
     justifyContent: "space-around",
     flexDirection: "column",
     backgroundColor: "gray",
-    height: "20vh"
+    height: "20vh",
   } as CSSProperties
   
+  function handleServerSwitch() {
+    setServerID((document.getElementById("serverSelect") as unknown as HTMLSelectElement)!.value)
+  }
 
   return (
     <>
@@ -108,16 +120,20 @@ export default function Page() {
             <hr style={{borderWidth: "1vh", borderColor: "black"}}/>
             {chatList.map((message, key)=>(
                 <div key={key}>
-                  <hr />
                   sent by {message.username} at {message.sent_on}
-                  <div style={{backgroundColor: "darkgray"}}>{message.message}</div>
-                  <hr />
+                  <div style={{backgroundColor: "darkgray", borderRadius: "8px", marginLeft: "3%", marginRight: "3%"}}><p style={{marginLeft: "3%", marginRight: "3%"}}>{message.message}</p></div>
                 </div>
             ))}
           </div>
           <hr style={{borderWidth: "1vh", borderColor: "black"}}/>
           <div style={scrolldivS}>
-            there is only this server
+            <div>server list</div>
+            <select name="serverSelect" id="serverSelect" onChange={() => handleServerSwitch()} style={{marginLeft: "3%", marginRight: "3%"}} >
+              <option value={serverData[0].id}>Server selection</option>
+              {serverList.map((server, key)=>(
+                <option key={key} value={server.id}>{server.name}</option>
+              ))}
+            </select>
           </div>
         </div>
         <div style={sidedivR}>
