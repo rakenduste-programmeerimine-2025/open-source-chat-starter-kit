bf911256677c60aec63ee4672a1bc39ffbe64967
marmarsell <marsell@tlu.ee>
2025-12-04 17:36:02 +0200

    refactor: Did code cleaning to avoid split second renders of all messages

--- app/me/chat/page.tsx
+++ app/me/chat/page.tsx
@@ -4,16 +4,28 @@ import { CSSProperties, useEffect, useState } from "react";
 
 export default function Page() {
 
-  type Message = {
-    deleted_at: null
-    edited_at: null
-    id: number
-    message: string
-    sender_id: string
-    sent_on: string
-    server_id: string
-    username: string
-  }
+  type Messages = [
+    message:{
+      deleted_at: null
+      edited_at: null
+      id: number
+      message: string
+      sender_id: string
+      sent_on: string
+      server_id: string
+      username: string
+    }
+  ]
+
+  type ServList = [
+    server: {
+      id: string
+      name: string
+      image_url: null
+      created_by: string
+      created_at: string
+    }
+  ]
 
   type ServData = {
     id: string
@@ -23,27 +35,28 @@ export default function Page() {
     created_at: string
   }
 
-  const [chatList, setChatList] = useState([[{}]]) as unknown as Array<Array<Message>>
+  const [chatList, setChatList] = useState([[{}]]) as unknown as Array<Messages>
   const [serverData, setServerData] = useState([[{}]]) as unknown as Array<Array<ServData>>
 
-  const [serverID, setServerID] = useState("f291a9a9-10aa-4057-acdc-7e036d7111ac") //hardcoded lmfaogffs
+  const [serverID, setServerID] = useState("f291a9a9-10aa-4057-acdc-7e036d7111ac")
+  //const [serverID, setServerID] = useState("36d2abfa-2d13-48f9-b701-78e355c89f21")
+
+  const [updateCounter, setUpdateCounter] = useState(0)
 
   useEffect(() => {
     fetch('http://localhost:3000/api/messages/' + serverID)
       .then(response => response.json())
       .then(response => setChatList(response.data))
-  }, [])
 
-  useEffect(() => {
     fetch('http://localhost:3000/api/servers/' + serverID)
       .then(response => response.json())
       .then(response => setServerData(response))
-  }, [])
+  }, [serverID])
 
-  useEffect(()=>{
-    console.log(chatList)
-    console.warn(serverData)
-  },[chatList, serverData])
+  useEffect(() => {
+    console.warn(updateCounter)
+    console.log(serverData, chatList)
+  }, [serverData && chatList])
   
 
   const window = {
